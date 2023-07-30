359c236a77a8328d91b20413b76fadc202f48c4f
Oskar Schulz <vollkorncrafter@gmail.com>
2023-07-30 20:08:32 +0200

    viele changes

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ import random
 from voxypy.models import Entity, Voxel
 import PIL.Image as im
 
-
-ti.init(arch=ti.vulkan)
+ti.init(arch=ti.opengl)
 
 vec3 = ti.math.vec3
 
-
-
-
-entity = Entity().from_file("ffp\\tree.vox")
-
+entity = Entity().from_file("ffp\\test.vox")
 
 print(entity.get(1,1,1))
 
@@ -68,24 +63,22 @@ class hit:
 @ti.func
 def ray_color(ray_origin, ray_direction):
     t = 0.5 * (tm.normalize(ray_direction).y + 1)
-    return ((1.0 - t) * tm.vec3(1,1,1) + t * tm.vec3(0.5, 0.7, 1.0))
-    #return vec3(0)
-
+    return (((1.0 - t) * tm.vec3(1,1,1) + t * tm.vec3(0.5, 0.7, 1.0))) / 2
 
 @ti.func
 def random_in_unit_sphere():
   theta = 2.0 * 3.14 * ti.random()
   phi = ti.acos((2.0 * ti.random()) - 1.0)
   r = ti.pow(ti.random(), 1.0 / 3.0)
-  return ti.Vector([r * ti.sin(phi) * ti.cos(theta), r * ti.sin(phi) * ti.sin(theta), r * ti.cos(phi)])
+  return ti.Vector([r * ti.sin(phi) * ti.cos(theta), r * ti.sin(phi) * ti.sin(theta), r * ti.cos(phi)]) 
 
 
 
 eps = 1e-4
 inf = 1e10
 
-image_width = 512
-image_height = 512
+image_width = 1220
+image_height = 720
 
 aspect = image_width/image_height
 
@@ -98,17 +91,33 @@ horizontal = tm.vec3(viewport_width, 0, 0)
 vertical = tm.vec3(0, viewport_height, 0)
 lower_left_corner = origin - horizontal / 2 - vertical / 2 - tm.vec3(0, 0, focal_length)
 
-texture = ti.field(dtype=tm.vec3, shape=(16, 16))
+texture = ti.field(dtype=tm.vec3, shape=(48, 16))
 
-im = im.open('ffp\grass_block_side.png')
-rgb_im = im.convert('RGB')
-
-
+img = im.open('ffp\sand.png')
+rgb_im = img.convert('RGB')
 
 for x in range(16):
     for y in range(16):
         r, g, b = rgb_im.getpixel((x, y))
         texture[x,y] = vec3(r,g,b) / 255
+        
+
+img = im.open('ffp\sand_s.png')
+rgb_im = img.convert('RGB')
+
+for x in range(16):
+    for y in range(16):
+        r, g, b = rgb_im.getpixel((x, y))
+        texture[x+16,y] = vec3(r,g,b) / 255
+        
+        
+img = im.open('ffp\sand_n.png')
+rgb_im = img.convert('RGB')
+
+for x in range(16):
+    for y in range(16):
+        r, g, b = rgb_im.getpixel((x, y))
+        texture[x+32,y] = vec3(r,g,b) / 255
 
 
 
@@ -123,7 +132,7 @@ voxels = Voxel.field(shape=(s))
 
 print(zmax)
 
-palette = entity.get_palette()
+#palette = entity.get_palette()
 
 chunk = ti.field(dtype=ti.types.i32, shape=(xmax,zmax,ymax))
 color = ti.field(dtype=vec3, shape=(xmax,zmax,ymax))
@@ -134,8 +143,8 @@ for x in range(xmax):
         for z in range(zmax):
             if(entity.get(x,y,z) != 0):
                 chunk[x,z,y] = 1
-                f = vec3(palette[entity.get(x,y,z)._color][0],palette[entity.get(x,y,z)._color][1],palette[entity.get(x,y,z)._color][2])/255
-                color[x,z,y] = f
+                #f = vec3(palette[entity.get(x,y,z)._color][0],palette[entity.get(x,y,z)._color][1],palette[entity.get(x,y,z)._color][2])/255
+                #color[x,z,y] = f
                 print(str(x) + " " + str(y) + " " + str(z))
 
 
@@ -154,68 +163,57 @@ def get_cube_normal(nx, ny, nz):
 
 
 @ti.func
-def scene_sdf(p):
-    min_dist = inf
-
-    for x in range(tm.clamp(int(p.x)-16,0,xmax),tm.clamp(int(p.x)+16,0,xmax)):
-        for y in range(tm.clamp(int(p.y)-16,0,xmax),tm.clamp(int(p.y)+16,0,xmax)):
-            for z in range(tm.clamp(int(p.z)-16,0,xmax),tm.clamp(int(p.z)+16,0,xmax)):
-                if chunk[x, y, z] > 0:
-                    voxel_min = vec3(x, y, z) - 0.5
-                    voxel_max = vec3(x, y, z) + 0.5
-                    dist_x = tm.max(voxel_min.x - p.x, p.x - voxel_max.x)
-                    dist_y = tm.max(voxel_min.y - p.y, p.y - voxel_max.y)
-                    dist_z = tm.max(voxel_min.z - p.z, p.z - voxel_max.z)
-
-                    # Use the maximum of the component-wise distances to get the signed distance
-                    dist = tm.max(dist_x, tm.max(dist_y, dist_z))
-
-                    if dist < min_dist:
-                        min_dist = dist
-
-    return min_dist
-
-
-@ti.func
 def intersection(o, d):
-    max_distance = inf 
-    distance = 0.1
-    min_dist = inf
-    p = vec3(0)
-    nx = 0
-    ny = 0
-    nz = 0
-    normal = vec3(0)
-    p2 = vec3(0)
-    for i in range(48000):
-        p = o + d * distance
-        nx = int(tm.round(p.x))
-        ny = int(tm.round(p.y))
-        nz = int(tm.round(p.z))
-        cur_dist = 0.0
-        if 0 <= nx < xmax and 0 <= ny < ymax and 0 <= nz < zmax:
-            if chunk[nx, ny, nz] == 1:
-                cur_dist = tm.distance(o,p)
-                if cur_dist < min_dist:
-                    min_dist = cur_dist
-                    normal = (vec3(nx,ny,nz) - (p + (d * -1 * 0.1)))
-                    normal=get_normal(normal) * -1
-                    p2 = p
-                    break
+    d = tm.normalize(d)
+    X = int(tm.round(o.x))
+    Y = int(tm.round(o.y))
+    Z = int(tm.round(o.z))
+
+    stepX = int(tm.sign(d.x))
+    stepY = int(tm.sign(d.y))
+    stepZ = int(tm.sign(d.z))
+
+    tMaxX = (X + (0.5 if stepX > 0 else -0.5) - o.x) / d.x
+    tMaxY = (Y + (0.5 if stepY > 0 else -0.5) - o.y) / d.y
+    tMaxZ = (Z + (0.5 if stepZ > 0 else -0.5) - o.z) / d.z
+    tMax = 0.0
+    tDeltaX = abs(1 / d.x)
+    tDeltaY = abs(1 / d.y)
+    tDeltaZ = abs(1 / d.z)
+
+    voxel = 0
+    D = 0.0 
+
+    for i in range(100):
+        if tMaxX < tMaxY and tMaxX < tMaxZ:
+            tMax = tMaxX
+            X += stepX
+            tMaxX += tDeltaX
+            if X == xmax or X < 0:
+                break
+        elif tMaxY < tMaxZ:
+            tMax = tMaxY
+            Y += stepY
+            tMaxY += tDeltaY
+            if Y == ymax or Y < 0:
+                break
         else:
+            tMax = tMaxZ
+            Z += stepZ
+            tMaxZ += tDeltaZ
+            if Z == zmax or Z < 0:
+                break
+
+        voxel = chunk[X, Y, Z] if 0 <= X < xmax and 0 <= Y < ymax and 0 <= Z < zmax else 0
+        if voxel > 0:
             break
-                    
-        distance += 0.01 # Reduce the step size for ray marching
-        if distance >= max_distance:
-            break
 
-    # Normale normieren (Länge 1)
-    normal = tm.normalize(normal)
+    if voxel > 0:
+        D = tMax
+    else:
+        D = 0
 
-    return hit(cord=p2, d=min_dist, normal=normal)
-
-
-
+    return hit(cord=o+d*(D-0.001), d=D, normal=get_normal((o+d*D)-vec3(X,Y,Z)))
 
 @ti.func
 def get_normal(surface_normal):
@@ -227,7 +225,7 @@ def get_normal(surface_normal):
             maximum = surface_normal[i]
     surface_normal = vec3(0)
     surface_normal[index] = maximum
-    return surface_normal
+    return tm.normalize(surface_normal)
 
 
 pixels = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
@@ -236,23 +234,50 @@ blur = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
 final = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
 
 albedo = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
+specular = ti.field(dtype=tm.vec3, shape=(image_width, image_height))#
 normals = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
 depth = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
 diffuse = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
 hit_point = ti.field(dtype=tm.vec3, shape=(image_width, image_height))
+sky = ti.field(dtype=tm.vec3, shape=(image_width, image_height))#
+
+@ti.func
+def texture_map(cord, normal,offset,spec):
+    x = int(ti.round(cord.x))
+    y = int(ti.round(cord.y))
+    z = int(ti.round(cord.z))
+    f = vec3(x - cord.x, y - cord.y, z - cord.z)
+    sign = tm.sign(f)
+    f = ti.abs((f + 0.5) * 16)
+    s = 1.0
+    if(ti.abs(normal.z) > 0):
+        f = (texture[int(f.x+offset), int(f.y)])
+        if(spec):
+            s = tm.sign(normal.z)
+    elif(ti.abs(normal.y) > 0):
+        f = (texture[int(f.x+offset), int(f.z)])
+        if(spec):
+            s = tm.sign(normal.y)
+    else:
+        f = (texture[int(f.z+offset), int(f.y)])
+        if(spec):
+            s = tm.sign(normal.x)
+
+    return f * s
+
+
+
+
 
 @ti.func
 def gbuff(o,d,i,j):
     hitrecord = intersection(o=o, d=d)
     if hitrecord.d > 0 and hitrecord.d != inf:
-        x = int(ti.round(hitrecord.cord.x))
-        y = int(ti.round(hitrecord.cord.y))
-        z = int(ti.round(hitrecord.cord.z))
-        f = vec3(x-hitrecord.cord.x+0.5,y-hitrecord.cord.y+0.5,z-hitrecord.cord.z+0.5)*16
-        albedo[i,j] = texture[int(f.x),int(f.y)]
         normal = hitrecord.normal
+        albedo[i,j] = texture_map(hitrecord.cord,normal,0,False)
+        specular[i,j] = texture_map(hitrecord.cord,normal,16,False)
 
-        normals[i,j] = hitrecord.normal
+        normals[i,j] = normal
         depth[i,j] = hitrecord.d 
         hit_point[i,j] = hitrecord.cord
     else:
@@ -261,32 +286,83 @@ def gbuff(o,d,i,j):
         normals[i, j] = vec3(0)
         albedo[i,j] = vec3(inf)
         hit_point[i,j] = vec3(0)
+        specular[i,j] = vec3(0)
 
+
+@ti.func
+def bilateral_filter(sigma_s: ti.f32, sigma_r: ti.f32):
+
+    blur_radius_s = ti.ceil(sigma_s * 3, int)
+
+    for i in range(image_width):
+        for j in range(image_height):
+            k_begin, k_end = max(0, i - blur_radius_s), min(image_width, i + blur_radius_s + 1)
+            l_begin, l_end = max(0, j - blur_radius_s), min(image_height, j + blur_radius_s + 1)
+            total_rgb = tm.vec3(0.0)
+            total_weight = 0.0
+            for k in range(k_begin,k_end):
+                for l in range(l_begin,l_end):
+                    dist = ((i - k)**2 + (j - l)**2) / sigma_s**2 + (diffuse[i, j].cast(ti.f32) - diffuse[k, l].cast(ti.f32)).norm_sqr() / sigma_r**2
+                    w = ti.exp(-0.5 * dist)
+                    total_rgb += diffuse[k, l] * w
+                    total_weight += w
+
+            blur[i, j] = (total_rgb / total_weight)
+
+@ti.func
+def lambertian_brdf(incoming_direction, outgoing_direction, surface_normal, diffuse_reflectance):
+    incoming_direction = tm.normalize(incoming_direction)
+    outgoing_direction = tm.normalize(outgoing_direction)
+    surface_normal = tm.normalize(surface_normal)
+
+    # Compute the cosine of the incident angle (angle between light direction and surface normal)
+    cos_incident_angle = tm.max(0, tm.dot(incoming_direction, surface_normal))
+
+    # Compute the Lambertian BRDF value
+    brdf_value = (diffuse_reflectance / 3.14) * cos_incident_angle
+
+    return brdf_value
+
+    
 @ti.func
 def render_diffuse(o, d, i, j):
     c = vec3(1)
     normal = normals[i, j]
-    o = hit_point[i,j]
-    d = (random_in_unit_sphere() + normal)
+    spec = specular[i,j].x
+    o = hit_point[i,j] + (normal * 0.02)
+    r = random_in_unit_sphere()
+    if tm.dot(normal, r) < 0:
+        r = r - 2 * tm.dot(normal, r) * normal
+    roughness = tm.pow(1 - spec, 2)
+    d = r * (roughness) + (tm.reflect(x=d,n=normal) * (1-roughness))
+    d = tm.normalize(d)
     if(depth[i,j].x > 0):
-        for b in range(10):
-            hitrecord = intersection(o=o, d=d)
-            if hitrecord.d > 0 and hitrecord.d != inf:
-                if(b == 9):
-                    c = vec3(0)
-                else:
+        for b in range(100):
+            if(b == 99):
+                c = vec3(0) 
+            else:
+                hitrecord = intersection(o=o, d=d)
+                if hitrecord.d > 0 and hitrecord.d != inf:
                     x = int(ti.round(hitrecord.cord.x))
                     y = int(ti.round(hitrecord.cord.y))
                     z = int(ti.round(hitrecord.cord.z))
                     f = vec3(x - hitrecord.cord.x + 0.5, y - hitrecord.cord.y + 0.5, z - hitrecord.cord.z + 0.5) * 16
-                    c = c * (texture[int(f.x), int(f.y)]) * (1 / hitrecord.d*hitrecord.d)
+                    nc = texture_map(hitrecord.cord,normal,0,False)
+                    id = d
+                    spec = texture_map(hitrecord.cord,normal,16,False).x
                     normal = hitrecord.normal                    
-                    o = hitrecord.cord + (normal * 0.01)  # Move the origin to the hit point
-                    d = ((random_in_unit_sphere() + normal))
-                    d = tm.normalize(d)  # Generate a random direction for the next ray
-            else:
-                c = c * ray_color(ray_origin=o,ray_direction=d)
-                break
+                    o = hitrecord.cord + (normal * 0.02)  # Move the origin to the hit point
+                    r = random_in_unit_sphere()
+                    if tm.dot(normal, r) < 0:
+                        r = r - 2 * tm.dot(normal, r) * normal
+                    roughness = tm.pow(1 - spec, 2)
+                    d = r  * (1-roughness)  + (tm.reflect(x=d,n=normal) * (1-roughness))
+                    d = tm.normalize(d)
+                    c = c * (nc * lambertian_brdf(incoming_direction=id,outgoing_direction=d,surface_normal=normal,diffuse_reflectance=roughness))
+
+                else:
+                    c = c * (ray_color(ray_origin=o,ray_direction=d)* lambertian_brdf(incoming_direction=id,outgoing_direction=d,surface_normal=normal,diffuse_reflectance=roughness))
+                    break
     else:
         c = vec3(0)
     return c
@@ -296,79 +372,69 @@ def render_specular(o, d, i, j):
     c = vec3(1)
     normal = normals[i, j]
     o = hit_point[i,j]
-    d = tm.reflect(x=d,n=normal)
+    d = tm.reflect(x=d,n=normal*-1)
     if(depth[i,j].x > 0):
-        for b in range(24):
+        for b in range(1):
             hitrecord = intersection(o=o, d=d)
             if hitrecord.d > 0 and hitrecord.d != inf:
-                if(b == 23):
+                if(b == 1):
                     c = vec3(0)
                 else:
                     x = int(ti.round(hitrecord.cord.x))
                     y = int(ti.round(hitrecord.cord.y))
                     z = int(ti.round(hitrecord.cord.z))
+                    normal = hitrecord.normal       
                     f = vec3(x - hitrecord.cord.x + 0.5, y - hitrecord.cord.y + 0.5, z - hitrecord.cord.z + 0.5) * 16
-                    c = c * (texture[int(f.x), int(f.y)]) * (1 / hitrecord.d*hitrecord.d)
-                    normal = hitrecord.normal                    
-                    o = hitrecord.cord + (normal * 0.01)  # Move the origin to the hit point
+                    o = hitrecord.cord  + (normal*0.1)  # Move the origin to the hit point
                     d = tm.reflect(x=d,n=normal)
+                    c = c * (texture[int(f.x), int(f.y)])
+                    
             else:
-                c = c * ray_color(ray_origin=o,ray_direction=d)
+                c = c * (ray_color(ray_origin=o,ray_direction=d)/2)
                 break
     else:
         c = vec3(0)
     return c
 
 
-
-@ti.func
-def trace(o, d, i, j):
-    c = vec3(1)
-    normal = vec3(0)
-    for b in range(1):
-        hitrecord = intersection(o=o, d=d)
-        if hitrecord.d > 0 and hitrecord.d != inf:
-            x = int(ti.round(hitrecord.cord.x))
-            y = int(ti.round(hitrecord.cord.y))
-            z = int(ti.round(hitrecord.cord.z))
-            #c = c * color[x,y,z]
-            f = vec3(x-hitrecord.cord.x+0.5,y-hitrecord.cord.y+0.5,z-hitrecord.cord.z+0.5)*16
-            c = c * texture[int(f.x),int(f.y)]
-            normal = hitrecord.normal
-            o = hitrecord.cord + (normal * 0) # Move the origin to the hit point
-            d =  (tm.reflect(x=d,n=normal) * 1) + ((random_in_unit_sphere() + normal) * 0.1)
-            d = tm.normalize(d)  # Generate a random direction for the next ray
-            if(b==0):
-                normals[i,j] = normal
-                depth[i,j] = (16 - hitrecord.d) / 16 
-        else:
-            c = c * ray_color(ray_origin=o, ray_direction=d)  
-            if(b==0):
-                depth[i, j] = vec3(0)
-                normals[i, j] = vec3(0)
-            break
-    return c
-
+vel = ti.field(dtype=tm.vec3, shape=(image_width, image_height))#
+last = ti.field(dtype=tm.vec3, shape=(image_width, image_height))#
+test = ti.field(dtype=tm.vec3, shape=(image_width, image_height))#
 
 
 
 @ti.kernel
-def paint(o: vec3,w: int):
+def paint(o: vec3,w: int, rot: vec3):
+    angle = rot.y * (3.14 / 180)  # Rotate 45 degrees
+
+# Create the rotation matrix using the math module
+    rot_matrix = ti.Matrix([[tm.cos(angle), 0, tm.sin(angle)],
+                            [0.0, 1, 0.0],
+                            [-tm.sin(angle), 0.0, tm.cos(angle)]])
+
     for i, j in pixels:
         u = i / (image_width - 1)
         v = j / (image_height - 1)
         d =  lower_left_corner + u * horizontal + v * vertical 
-        dif = vec3(0)
+        d = rot_matrix @ d
         gbuff(o=o,d=d,i=i,j=j)
-        for s in range(2):
-            dif += render_diffuse(o=o,d=d,i=i,j=j) 
-        diffuse[i,j] = dif / 2 + render_specular(o=o,d=d,i=i,j=j) * 0.4
+        dif = vec3(0)
+        for sa in range(6):
+            dif += render_diffuse(o,d,i,j) 
+            diffuse[i,j] = render_specular(o,d,i,j) * 0.0 + (dif / 6)
+        c = vec3(0)
+        if (normals[i,j].x == 0 and normals[i,j].y == 0 and normals[i,j].z == 0):
+            c = ray_color(ray_direction=d,ray_origin=o)
+        sky[i,j] = c/2
+        
     for i, j in pixels:
-        pixels[i,j] = albedo[i,j] * diffuse[i,j]
+        pixels[i,j] = ((albedo[i,j] * diffuse[i,j]) + last[i,j]) / 2 + sky[i,j]
     
+    for i, j in last:
+        last[i,j] = pixels[i,j]
     
 gui = ti.GUI("render", res=(image_width, image_height),fast_gui=True)
-diffuse_gui = ti.GUI("diffuse", res=(image_width, image_height),fast_gui=False)
+diffuse_gui = ti.GUI("diffuse", res=(image_width, image_height),fast_gui=True)
 
 w = 1
 
@@ -377,12 +443,13 @@ w = 1
 print(xmax)
 print(ymax)
 print(zmax)
+rot = vec3(0)
 while gui.running:
     gui.get_event()  # must be called before is_pressed
     if gui.is_pressed('a', ti.GUI.LEFT):
-        origin.x += - 0.1
+        rot.y += - 1
     elif gui.is_pressed('d', ti.GUI.RIGHT):
-        origin.x += 0.1
+        rot.y += 1
     elif gui.is_pressed('w', ti.GUI.UP):
         origin.z += - 0.1
     elif gui.is_pressed('s', ti.GUI.DOWN):
@@ -390,7 +457,7 @@ while gui.running:
     
     origin.y = 2
     gui.show()
-    paint(origin,w=w+1)
+    paint(origin,w=w+1,rot=rot)
     gui.set_image(pixels)
     gui.show()
     diffuse_gui.set_image(diffuse)
